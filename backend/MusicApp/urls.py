4f19d54747211deb33eabac5def46e38830545da
Wilson McCloy <wilsonmccloy@Wilsons-MacBook-Air.local>
2022-04-02 13:47:43 -0400

    backend

--- backend/MusicApp/urls.py
+++ backend/MusicApp/urls.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, ArtistViewSet, RatingViewSet, AttributeViewSet
+#from .views import UserViewSet, ArtistViewSet, RatingViewSet, AttributeViewSet
 from . import views
 
 
-router = routers.DefaultRouter()
-router.register('user', UserViewSet)
-router.register('artist', ArtistViewSet)
-router.register('rating', RatingViewSet)
-router.register('attribute', AttributeViewSet)
+# router = routers.DefaultRouter()
+# router.register('user', UserViewSet)
+# router.register('artist', ArtistViewSet)
+# router.register('rating', RatingViewSet)
+# router.register('attribute', AttributeViewSet)
 
 
 #set up the different paths availible for the users
 app_name = "MusicApp"
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    #path('api/', include(router.urls)),
     path('', views.index, name='index'),
     path('registration/', views.user_registration, name='registration'),
     path('songretrieval/', views.song_retrieval, name='songretrieval'),
