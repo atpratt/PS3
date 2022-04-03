baa868c8e4c6b524dc83f999ce00a40f00852bf6
Wilson McCloy <wilsonmccloy@Wilsons-MacBook-Air.local>
2022-04-03 16:06:15 -0400

    login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App'; //this is refering to the APP.js file
 import reportWebVitals from './reportWebVitals';
 //import {Route, BrowswerRoute as Router, Switch} from 'react-router-dom' 
