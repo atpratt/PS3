488e4523151324adc08672dfa0855b26a30c62ef
atpratt <98915134+atpratt@users.noreply.github.com>
2022-04-07 17:15:52 -0400

    update rating so pushed to db. Only correct users so fat

--- frontend/src/components/ratingModal.js
+++ frontend/src/components/ratingModal.js
@@ -1,7 +1,8 @@
-// frontend/src/components/Modal.js
+//RATING MODAL
 
+import axios from "axios";
 import React from "react";
-// We would like to use a modal (small window) to show details of a task.
+
 import {
   Button,
   Modal,
@@ -14,53 +15,70 @@ import {
   Label,
 } from "reactstrap";
 
-// We are creating and exporting the CustomModal class.
-// It will be imported in our App.js.
 export default class CustomModal extends React.Component {
   constructor(props) {
     super(props);
     // The state object is initialized in the constructor.
-    // It can contain as many properties as you like.
-    // Here we define one property called activeItem.
+    // here we are setting ther activeRatingx
     this.state = {
       activeRating: this.props.activeRating,
     };
   }
 
-  // Whenver we enter changes into our form, e.g., for the title of our task,
-  // we want the change to be immediately detected. This is what happens when
-  // this function is called below with onChange={this.handleChange}.
-  // handleChange takes an event argument, i.e., a change in title, descripton,
-  // or checkbox.
   handleChange = (event) => {
-    // An event has a target, thus, event.target gives us the event's DOM element, e.g.,
-    // <input name="description" placeholder="Enter Todo description" type="text" class="form-control" value="My Task">.
-    // From that, we assign the variables name = "description" and value = "My Task".
     let { name, value } = event.target;
-    if (event.target.type === "checkbox") {
-      value = event.target.checked;
-    }
-    // Recall the spread operator three dots ... , e.g.,
-    // var parts = ['two', 'three']; // [two", "three"]
-    // var numbers = ['one', ...parts, 'four']; // ["one", "two", "three", "four"]
-    // ...this.state.activeItem spreads out the current task object, e.g.,
-    // {"id": 3, "title": "My Task", "description": "Wash Dishes", "completed": true}.
-    // [name]: value sets the name of the task to the new value the user entered
-    // (e.g., title to "Another Task" or description to "Sweep floor").
+    // if (event.target.type === "checkbox") {
+    //   value = event.target.checked;
+    // }
     const activeRating = { ...this.state.activeRating, [name]: value };
-    // To change a value in the `state` object for rendering, use the `this.setState()`
-    // method. If you would not do so, the text shown to the user in the textbox, e.g.,
-    // for the title of the new task would not change.
     this.setState({ activeRating });
+    // console.log(this.state.activeRating.username);
+  };  
+
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/todos/")
+      .then(res => this.setState({ activeRating: res.data }))
+      .catch(err => console.log(err));
   };
-  // The `render()` method is the only required method in a class component.
-  // When called, it will render the page. You do not have to specifically
-  // call render() in your component. Rather, the stub code with the
-  // ReactDOM.render(...) in your index.js will do that for you.
-  // The following will render the modal for adding or editing a task.
+
+  ratingSubmit = ar => {
+    // If the item does not yet exist: POST Request 
+    axios
+    .post("http://localhost:8000/api/rating/", ar)
+    //.then(res => console.log(res));
+    .then(res => this.refreshList());
+
+  };
+
+  // DELETE.
+  ratingDelete = ar => {
+    axios
+      .delete(`http://localhost:8000/api/rating/${ar.id}`)
+      .then(res => this.refreshList());
+  };
+
+  //EDIT the rating and submits it again
+  editRating = ar => {
+    this.setState({ activeRating: ar, modal: !this.state.modal });
+    this.ratingSubmit(this.state.activeRating)
+  };
+
+
+// HTTP Request Functions
+// Create - Rating Submit 
+// Read
+// Update
+// Delete
+
+
+
+
+
+
+
   render() {
-    // The modal has three properties: toggle, onSave, and activeItem.
-    // See App.js.
+    // The modal has three properties: toggle, onSave, and activeRating.
     const { toggle, onSave } = this.props;
     return (
       <Modal isOpen={true} toggle={toggle}>
@@ -102,7 +120,9 @@ export default class CustomModal extends React.Component {
           </Form>
         </ModalBody>
         <ModalFooter>
-          <Button color="success" onClick={() => onSave(this.state.activeRating)}>
+          <Button onClick={() => this.ratingSubmit(this.state.activeRating)}>
+            {/* onClick={this.createItem} > */}
+            {/* onClick={ratingSubmit}> */}
             Save
           </Button>
         </ModalFooter>
